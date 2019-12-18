e309a4c1b76a08ee19099e7361198c4968aad545
Raí Tamarindo <raitamarindo@gmail.com>
2019-12-18 13:53:01 -0300

    Add resource endpoint

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"strings"
@@ -42,7 +43,8 @@ func serve(config config) {
 		jwkID:      config.jwkID,
 	}
 
-	http.HandleFunc("/validate", auth.validate)
+	http.Handle("/validate", auth.validate(emptyHandler{}))
+	http.Handle("/api-resource", auth.validate(resourceHandler{}))
 
 	err := http.ListenAndServe(":"+config.httpPort, nil)
 	if err != nil {
@@ -51,35 +53,51 @@ func serve(config config) {
 
 }
 
+type resourceHandler struct{}
+
+func (h resourceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+	fmt.Fprintf(w, "This is a secret number: %d", random.Intn(1000000000))
+}
+
+type emptyHandler struct{}
+
+func (h emptyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
+
 type authenticationMiddleware struct {
 	jwksClient jwks.JWKSClient
 	jwkID      string
 }
 
-func (m authenticationMiddleware) validate(w http.ResponseWriter, r *http.Request) {
-	authHeader := r.Header.Get("Authorization")
-	if !strings.HasPrefix(strings.ToLower(authHeader), "bearer ") {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
+func (m authenticationMiddleware) validate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if !strings.HasPrefix(strings.ToLower(authHeader), "bearer ") {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
 
-	jws, err := jose.ParseSigned(authHeader[7:])
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Println(err.Error())
-		return
-	}
-	jwk, err := m.jwksClient.GetEncryptionKey(m.jwkID)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Println(err.Error())
-		return
-	}
+		jws, err := jose.ParseSigned(authHeader[7:])
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println(err.Error())
+			return
+		}
+		jwk, err := m.jwksClient.GetEncryptionKey(m.jwkID)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println(err.Error())
+			return
+		}
 
-	if _, err := jws.Verify(jwk.Key); err != nil {
-		w.WriteHeader(http.StatusUnauthorized)
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintf(w, `{"error":"%s"}`, err.Error())
-		return
-	}
+		if _, err := jws.Verify(jwk.Key); err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Header().Set("Content-Type", "application/json")
+			fmt.Fprintf(w, `{"error":"%s"}`, err.Error())
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
